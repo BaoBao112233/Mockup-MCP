fc893593e59019beef022555d43ffc33cbe26dd1
BaoBao112233 <kevinbao15072002@gmail.com>
2025-11-05 10:08:24 +0700

    [Mockup] Update data mockup

--- tools/mockup_data.py
+++ tools/mockup_data.py
@@ -3,132 +3,404 @@
 MOCK_TOKEN = "mock_token_abc123xyz"
 
 MOCK_ROOMS = [
-    {
-        "house_id": 1,
-        "room_id": 101,
-        "room_name": "Phòng khách",
-        "devices": [
-            {
-                "name": "Switch SH4",
-                "seriNumber": "SN001",
-                "device_status": "Đang kết nối"
-            }
-        ],
-        "buttons": [
-            {
-                "buttonId": 1001,
-                "name": "Quạt điện",
-                "button_code": "0101",
-                "button_type": "FAN",
-                "label": "FAN",
-                "modelName": "Switch-4",
-                "remoteIRId": None,
-                "brandId": None,
-                "status": "tắt",
-                "deviceId": 201,
-                "seriNumber": "SN001",
-                "joinMesh": 1,
-                "net_Index": 1,
-                "app_Index": 1
-            },
-            {
-                "buttonId": 1002,
-                "name": "Điều hòa",
-                "button_code": "0102",
-                "button_type": "CONDITIONER",
-                "label": "CONDITIONER",
-                "modelName": "Daikin",
-                "remoteIRId": 301,
-                "brandId": 10,
-                "status": "tắt",
-                "deviceId": 201,
-                "seriNumber": "SN001",
-                "joinMesh": 1,
-                "net_Index": 1,
-                "app_Index": 1
-            },
-            # {
-            #     "buttonId": 1003,
-            #     "name": "Bình nóng lạnh",
-            #     "button_code": "0103",
-            #     "button_type": "HOT_COLD_SHOWER",
-            #     "label": "HOT_COLD_SHOWER",
-            #     "modelName": "Switch-4",
-            #     "remoteIRId": None,
-            #     "brandId": None,
-            #     "status": "tắt",
-            #     "deviceId": 201,
-            #     "seriNumber": "SN001",
-            #     "joinMesh": 1,
-            #     "net_Index": 1,
-            #     "app_Index": 1
-            # },
-            # {
-            #     "buttonId": 1004,
-            #     "name": "Công tắc đèn",
-            #     "button_code": "0104",
-            #     "button_type": "LIGHT",
-            #     "label": "LIGHT",
-            #     "modelName": "Switch-4",
-            #     "remoteIRId": None,
-            #     "brandId": None,
-            #     "status": "tắt",
-            #     "deviceId": 201,
-            #     "seriNumber": "SN001",
-            #     "joinMesh": 1,
-            #     "net_Index": 1,
-            #     "app_Index": 1
-            # },
-            # {
-            #     "buttonId": 1005,
-            #     "name": "Ổ cắm",
-            #     "button_code": "0105",
-            #     "button_type": "SOCKET",
-            #     "label": "SOCKET",
-            #     "modelName": "Switch-4",
-            #     "remoteIRId": None,
-            #     "brandId": None,
-            #     "status": "tắt",
-            #     "deviceId": 201,
-            #     "seriNumber": "SN001",
-            #     "joinMesh": 1,
-            #     "net_Index": 1,
-            #     "app_Index": 1
-            # },
-            # {
-            #     "buttonId": 1006,
-            #     "name": "Bếp điện",
-            #     "button_code": "0106",
-            #     "button_type": "SOCKET",
-            #     "label": "SOCKET",
-            #     "modelName": "Switch-4",
-            #     "remoteIRId": None,
-            #     "brandId": None,
-            #     "status": "tắt",
-            #     "deviceId": 201,
-            #     "seriNumber": "SN001",
-            #     "joinMesh": 1,
-            #     "net_Index": 1,
-            #     "app_Index": 1
-            # },
-            # {
-            #     "buttonId": 1007,
-            #     "name": "Khóa cửa",
-            #     "button_code": "0107",
-            #     "button_type": "LOCK",
-            #     "label": "LOCK",
-            #     "modelName": "Smart-Lock-01",
-            #     "remoteIRId": None,
-            #     "brandId": None,
-            #     "status": "khóa",
-            #     "deviceId": 201,
-            #     "seriNumber": "SN001",
-            #     "joinMesh": 1,
-            #     "net_Index": 1,
-            #     "app_Index": 1
-            # }
-        ]
-    }
+  {
+    "house_id": 121145,
+    "room_id": 123768,
+    "room_name": "Bed room",
+    "devices": [
+      {
+        "name": "Công tắc beacon hình CN, màu trắng",
+        "seriNumber": "MCTRADA1",
+        "device_status": "Đang kết nối"
+      },
+      {
+        "name": None,
+        "seriNumber": "DH00000894",
+        "device_status": "Đang kết nối"
+      },
+      {
+        "name": "Công tắc vuông đen 4 nút",
+        "seriNumber": "MCTHK5Q2D",
+        "device_status": "Đang kết nối"
+      },
+      {
+        "name": "Công tắc vuông đen 4 nút",
+        "seriNumber": "MCT1I1DT9",
+        "device_status": "Đang kết nối"
+      }
+    ],
+    "buttons": [
+      {
+        "buttonId": 1905,
+        "name": "Điều hòa",
+        "button_code": "air_conditioner",
+        "button_type": 6,
+        "label": "CONDITIONER",
+        "modelName": None,
+        "remoteIRId": None,
+        "brandId": None,
+        "status": "tắt",
+        "deviceId": 79865,
+        "seriNumber": "DH00000894",
+        "joinMesh": None,
+        "net_Index": None,
+        "app_Index": None
+      },
+      {
+        "buttonId": 1929,
+        "name": "Đèn đọc sách",
+        "button_code": "button04",
+        "button_type": 1,
+        "label": "LIGHT",
+        "modelName": None,
+        "remoteIRId": None,
+        "brandId": None,
+        "status": "tắt",
+        "deviceId": 162320,
+        "seriNumber": "MCTHK5Q2D",
+        "joinMesh": 1,
+        "net_Index": 121145,
+        "app_Index": 0
+      },
+      {
+        "buttonId": 1928,
+        "name": "Đèn giá sách",
+        "button_code": "button03",
+        "button_type": 1,
+        "label": "LIGHT",
+        "modelName": None,
+        "remoteIRId": None,
+        "brandId": None,
+        "status": "tắt",
+        "deviceId": 162320,
+        "seriNumber": "MCTHK5Q2D",
+        "joinMesh": 1,
+        "net_Index": 121145,
+        "app_Index": 0
+      },
+      {
+        "buttonId": 1927,
+        "name": "Đèn trần",
+        "button_code": "button02",
+        "button_type": 1,
+        "label": "LIGHT",
+        "modelName": None,
+        "remoteIRId": None,
+        "brandId": None,
+        "status": "tắt",
+        "deviceId": 162320,
+        "seriNumber": "MCTHK5Q2D",
+        "joinMesh": 1,
+        "net_Index": 121145,
+        "app_Index": 0
+      },
+      {
+        "buttonId": 1926,
+        "name": "Đèn ngủ",
+        "button_code": "button01",
+        "button_type": 1,
+        "label": "LIGHT",
+        "modelName": None,
+        "remoteIRId": None,
+        "brandId": None,
+        "status": "tắt",
+        "deviceId": 162320,
+        "seriNumber": "MCTHK5Q2D",
+        "joinMesh": 1,
+        "net_Index": 121145,
+        "app_Index": 0
+      },
+      {
+        "buttonId": 1936,
+        "name": "Điều khiển TV Samsung",
+        "button_code": None,
+        "button_type": 3,
+        "label": "TV",
+        "modelName": "SAMSUNG_RM_00741A",
+        "remoteIRId": 569,
+        "brandId": 2,
+        "status": "tắt",
+        "deviceId": 162306,
+        "seriNumber": "MCT1I1DT9",
+        "joinMesh": None,
+        "net_Index": 121145,
+        "app_Index": 0
+      },
+      {
+        "buttonId": 1935,
+        "name": "Điều khiển điều hòa KAROFI",
+        "button_code": None,
+        "button_type": 3,
+        "label": "CONDITIONER",
+        "modelName": "ELECTRA_AC",
+        "remoteIRId": 568,
+        "brandId": 1,
+        "status": "tắt",
+        "deviceId": 162306,
+        "seriNumber": "MCT1I1DT9",
+        "joinMesh": None,
+        "net_Index": 121145,
+        "app_Index": 0
+      },
+      {
+        "buttonId": 1934,
+        "name": "Điều khiển Quạt Livotec",
+        "button_code": None,
+        "button_type": 3,
+        "label": "FAN",
+        "modelName": "LIVOTEC",
+        "remoteIRId": 567,
+        "brandId": 27,
+        "status": "tắt",
+        "deviceId": 162306,
+        "seriNumber": "MCT1I1DT9",
+        "joinMesh": None,
+        "net_Index": 121145,
+        "app_Index": 0
+      },
+      {
+        "buttonId": 1665,
+        "name": "Đèn 4",
+        "button_code": "button04",
+        "button_type": 1,
+        "label": "LIGHT",
+        "modelName": None,
+        "remoteIRId": None,
+        "brandId": None,
+        "status": "tắt",
+        "deviceId": 162306,
+        "seriNumber": "MCT1I1DT9",
+        "joinMesh": 1,
+        "net_Index": 121145,
+        "app_Index": 0
+      },
+      {
+        "buttonId": 1664,
+        "name": "Đèn 3",
+        "button_code": "button03",
+        "button_type": 1,
+        "label": "LIGHT",
+        "modelName": None,
+        "remoteIRId": None,
+        "brandId": None,
+        "status": "tắt",
+        "deviceId": 162306,
+        "seriNumber": "MCT1I1DT9",
+        "joinMesh": 1,
+        "net_Index": 121145,
+        "app_Index": 0
+      },
+      {
+        "buttonId": 1663,
+        "name": "Đèn 2",
+        "button_code": "button02",
+        "button_type": 1,
+        "label": "LIGHT",
+        "modelName": None,
+        "remoteIRId": None,
+        "brandId": None,
+        "status": "tắt",
+        "deviceId": 162306,
+        "seriNumber": "MCT1I1DT9",
+        "joinMesh": 1,
+        "net_Index": 121145,
+        "app_Index": 0
+      },
+      {
+        "buttonId": 1662,
+        "name": "Đèn 1",
+        "button_code": "button01",
+        "button_type": 1,
+        "label": "LIGHT",
+        "modelName": None,
+        "remoteIRId": None,
+        "brandId": None,
+        "status": "bật",
+        "deviceId": 162306,
+        "seriNumber": "MCT1I1DT9",
+        "joinMesh": 1,
+        "net_Index": 121145,
+        "app_Index": 0
+      }
+    ]
+  },
+  {
+    "house_id": 121145,
+    "room_id": 123672,
+    "room_name": "Thiết bị chưa gán phòng",
+    "devices": [
+      {
+        "name": "Công tắc vuông đen 4 nút",
+        "seriNumber": "MCT8LYTGT",
+        "device_status": "Đang kết nối"
+      }
+    ],
+    "buttons": [
+      {
+        "buttonId": 1933,
+        "name": "Nút bấm 4",
+        "button_code": "button04",
+        "button_type": 1,
+        "label": "LIGHT",
+        "modelName": None,
+        "remoteIRId": None,
+        "brandId": None,
+        "status": "tắt",
+        "deviceId": 162297,
+        "seriNumber": "MCT8LYTGT",
+        "joinMesh": 1,
+        "net_Index": 121145,
+        "app_Index": 0
+      },
+      {
+        "buttonId": 1932,
+        "name": "Nút bấm 3",
+        "button_code": "button03",
+        "button_type": 1,
+        "label": "LIGHT",
+        "modelName": None,
+        "remoteIRId": None,
+        "brandId": None,
+        "status": "tắt",
+        "deviceId": 162297,
+        "seriNumber": "MCT8LYTGT",
+        "joinMesh": 1,
+        "net_Index": 121145,
+        "app_Index": 0
+      },
+      {
+        "buttonId": 1931,
+        "name": "Nút bấm 2",
+        "button_code": "button02",
+        "button_type": 1,
+        "label": "LIGHT",
+        "modelName": None,
+        "remoteIRId": None,
+        "brandId": None,
+        "status": "tắt",
+        "deviceId": 162297,
+        "seriNumber": "MCT8LYTGT",
+        "joinMesh": 1,
+        "net_Index": 121145,
+        "app_Index": 0
+      },
+      {
+        "buttonId": 1930,
+        "name": "Nút bấm 1",
+        "button_code": "button01",
+        "button_type": 1,
+        "label": "LIGHT",
+        "modelName": None,
+        "remoteIRId": None,
+        "brandId": None,
+        "status": "tắt",
+        "deviceId": 162297,
+        "seriNumber": "MCT8LYTGT",
+        "joinMesh": 1,
+        "net_Index": 121145,
+        "app_Index": 0
+      }
+    ]
+  },
+  {
+    "house_id": 121145,
+    "room_id": 124538,
+    "room_name": "Phòng đọc sách",
+    "devices": [],
+    "buttons": []
+  },
+  {
+    "house_id": 121145,
+    "room_id": 124539,
+    "room_name": "Living room",
+    "devices": [],
+    "buttons": []
+  },
+  {
+    "house_id": 121145,
+    "room_id": 124446,
+    "room_name": "Ceiling Fan",
+    "devices": [
+      {
+        "name": "Công tắc chữ nhật trắng 4 nút",
+        "seriNumber": "MCTRF1",
+        "device_status": "Đang kết nối"
+      }
+    ],
+    "buttons": [
+      {
+        "buttonId": 1900,
+        "name": "bật đèn",
+        "button_code": "button04",
+        "button_type": 1,
+        "label": "LIGHT",
+        "modelName": None,
+        "remoteIRId": None,
+        "brandId": None,
+        "status": "tắt",
+        "deviceId": 225788,
+        "seriNumber": "MCTRF1",
+        "joinMesh": 1,
+        "net_Index": 121145,
+        "app_Index": 0
+      },
+      {
+        "buttonId": 1899,
+        "name": "tắt quạt",
+        "button_code": "button03",
+        "button_type": 1,
+        "label": "LIGHT",
+        "modelName": None,
+        "remoteIRId": None,
+        "brandId": None,
+        "status": "tắt",
+        "deviceId": 225788,
+        "seriNumber": "MCTRF1",
+        "joinMesh": 1,
+        "net_Index": 121145,
+        "app_Index": 0
+      },
+      {
+        "buttonId": 1898,
+        "name": "tắt đèn",
+        "button_code": "button02",
+        "button_type": 1,
+        "label": "LIGHT",
+        "modelName": None,
+        "remoteIRId": None,
+        "brandId": None,
+        "status": "tắt",
+        "deviceId": 225788,
+        "seriNumber": "MCTRF1",
+        "joinMesh": 1,
+        "net_Index": 121145,
+        "app_Index": 0
+      },
+      {
+        "buttonId": 1897,
+        "name": "bật quạt mức 6",
+        "button_code": "button01",
+        "button_type": 1,
+        "label": "LIGHT",
+        "modelName": None,
+        "remoteIRId": None,
+        "brandId": None,
+        "status": "tắt",
+        "deviceId": 225788,
+        "seriNumber": "MCTRF1",
+        "joinMesh": 1,
+        "net_Index": 121145,
+        "app_Index": 0
+      }
+    ]
+  },
+  {
+    "house_id": 121145,
+    "room_id": 123757,
+    "room_name": "Bed room 32",
+    "devices": [],
+    "buttons": []
+  }
 ]
 
 MOCK_DEVICE_PROPERTIES = {
